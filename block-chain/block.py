51482afade68bdb5857a6a0a62592dcd5f97200b
selengr <reza1997karbakhsh@gmail.com>
2026-06-21 15:42:43 +0330

    implements usage - start chain

--- block-chain/block.py
+++ block-chain/block.py
@@ -51,4 +51,20 @@ class Blockchain:
         )
         self.chain.append(new_block)
     
-    
+
+bc = Blockchain()
+
+tx1 = Transaction("Reza", "Bobi", 10)
+tx2 = Transaction("Bobi", "Charlie", 5)
+
+bc.add_block([tx1,tx2])
+
+tx3 = Transaction("Charlie", "Mahsa", 2)
+bc.add_block([tx3])
+
+for block in bc.chain:
+    print("Block:", block.index)
+    print("Hash:", block.hash)
+    print("Prev:", block.previous_hash)
+    print("Transactions:", [t.to_dict() for t in block.transactions])
+    print()
