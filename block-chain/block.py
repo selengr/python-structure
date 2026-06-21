844c82229928e62b0f9b879214e021683394fd13
selengr <reza1997karbakhsh@gmail.com>
2026-06-21 15:27:49 +0330

    comleted creating a single Block

--- block-chain/block.py
+++ block-chain/block.py
@@ -17,6 +17,24 @@ class Transaction:
         }
 
 class Block:
+    def __init__(self, index, transactions, previous_hash):
+        self.index = index
+        self.timestamp = time.time()
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+        
+    def calculate_hash(self):
+        print("transaction" + [t.to_dict() for t in self.transactions])
+        block_string = json.dumps({
+            "index" : self.index,
+            "timestamp" : self.timestamp,
+            "transactions" : [t.to_dict() for t in self.transactions],
+            "previous_hash" : self.previous_hash,
+        }, sort_keys=True).encode()
+        
+        return hashlib.sha256(block_string).hexdigest()
+        
 
 class Blockchain:
     
