d61f2a7a09654d8b79c2bc1ba7fd55b82e8b4b09
selengr <reza1997karbakhsh@gmail.com>
2026-06-22 16:21:30 +0330

    up

--- oop.py
+++ oop.py
@@ -1,14 +1,45 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def say_hello(self):
-        print("Hello, my name is", self.name)
+#     def say_hello(self):
+#         print("Hello, my name is", self.name)
 
 
-p1 = Person("Ali", 25)
+# p1 = Person("Ali", 25)
 
-p1.say_hello()
+# p1.say_hello()
 
-print(p1.age)
+# print(p1.age)
+
+# class Book:
+#     def __init__(self, page):
+#         self.pages = page
+
+
+# book = Book(250)
+
+# print(book.pages)
+
+
+
+
+
+
+
+# A small library of letters
+letters = {
+    'A': ["  *  ", " * * ", "*****", "*   *", "*   *"],
+    'B': ["****", "*   *", "****", "*   *", "****"]
+}
+
+name = input("Enter your name: ")
+first_char = name[0].upper()
+
+# Check if we have the letter in our "library"
+if first_char in letters:
+    for row in letters[first_char]:
+        print(row)
+else:
+    print("Sorry, I don't have a drawing for that letter yet.")
